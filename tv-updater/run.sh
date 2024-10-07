e25d255be25f89536ce88a911631ffbd00bbeabd
Evan Moses <evan.moses@okta.com>
2024-10-06 22:42:24 -0700

    Fix pwd for linux

--- tv-updater/run.sh
+++ tv-updater/run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker run -v $(PWD)/../scraper/output:/output --env-file  ../.env emoses/tv-updater $@
+docker run -v $(pwd)/../scraper/output:/output --env-file  ../.env emoses/tv-updater $@
